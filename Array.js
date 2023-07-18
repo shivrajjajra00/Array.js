50aaa4a8db4b82b185577a9ac5cb59dc29213649
shivrajjajra <shivrajjajra00@gmail.com>
2023-07-19 00:40:23 +0530

    Array file

---
+++ Array.js
@@ -0,0 +1,89 @@
+// Question 1. Array numbers ka. Loop chlana hai. Agar number odd hai to ODD print hai krna hai otherwise EVEN print krna hia 
+
+// let num = [1,2,3,4,5,6,7,8,9,7]
+// for(i = 0 ; i <= num.length;i++){
+//      if(num[i] % 2=== 0){
+// 		console.log(i,"Even");
+// 	}else{
+// 		console.log(i,"Odd");
+// 	}
+// }
+
+// Question 2. Array numbers ka. Sare numbers ko add krke print krna hai 
+
+// let num = [1,2,3,4,5,6,7,8,9]
+// blank = 0;
+// for(i = 0 ;i < num.length ;i++){
+// blank = blank + num[i]
+// }console.log(blank);
+
+// Question 3. Array numbers ka. Sare Odd numbers ko add krke print krna hai 
+    
+// let num = [1,2,3,4,5,6,7,8,9]
+// sumodd = 0;
+// for(i = 0 ; i < num.length ;i++){
+// 	if(num[i] % 2 !== 0){
+// 	 sumodd = sumodd+ num[i]
+// 	}
+// }console.log(sumodd);
+
+//Question 4. Array numbers ka. Sare Even numbers ko add krke print krna hai 
+
+// let num = [1,2,3,4,5,6,7,8,9]
+// sumodd = 0;
+// for(i = 0 ; i < num.length ;i++){
+// 	if(num[i] % 2 === 0){
+// 	 sumodd = sumodd+ num[i]
+// 	}
+// }console.log(sumodd);
+
+//Question 5. Array numbers ka. Maximum number btana hai 
+
+// let num = [1,2,3,4,5,6,7,8,9];
+// let maxNum = num[0];
+// for (let i = 1; i < num.length; i++) {
+//  if (num[i] > maxNum) {
+//     maxNum = num[i];
+//   }
+// }
+// console.log( maxNum);
+
+
+// Question 6. Array numbers ka. Minimum number btana hai 
+
+// let num = [1,2,3,4,5,6,7,8,9];
+// let minimumNum = num[0];
+// for (let i = 1; i < num.length; i++) {
+//  if (num[i] < minimumNum) {
+//     minimumNum = num[i];
+//   }
+// }
+// console.log( minimumNum);
+
+//Question 7. Array ka phla number print krvana hai 
+
+//  let num = [1,2,3,4,5,6,7,8,9];
+//  console.log(num[0]);
+
+//Question 8. Array ka last number print krvana hai
+
+// let num = [1,2,3,4,5,6,7,8,9];
+// console.log(num[num.length-1]);
+
+
+//Question 9. Array me kitne numbers aise hai jo 100 se bde hai 
+// let num = [1,2,3,4,5,116,7,8,119];
+// for(let i = 0; i < num.length; i++){
+//     if(num[i] > 100){
+//         console.log(num[i]);
+//     }
+// }
+
+
+// 10. Array me kitne numbers aise hai jo 20 se bde or Even hai 
+//  let num = [1,2,3,4,5,116,7,8,119];
+//  for(i = 0 ; i < num.length; i++){
+// 	if(num[i] < 20){
+// 		console.log(num[i]);
+// 	}
+//  }
